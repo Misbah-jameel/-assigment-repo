301d6b492f61e48dc8c23299ec28b6e310d9641c
MisbahDayo <dayomuhammad892@gmail.com>
2024-03-29 12:29:07 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+var myName = "Misbah";
+var myAge = 18;
+var myDeatail = "my Name is" + ' ' + myName + ' ' + "and my Age is" + ' ' + myAge;
+console.log(myDeatail);
---
+++ main.ts
@@ -0,0 +1,6 @@
+let myName:string = "Misbah";
+let myAge:number= 18;
+    let myDeatail : any = "my Name is" + ' ' +myName + ' ' + "and my Age is"+ ' ' + myAge ;
+
+ console.log ( myDeatail)
+
